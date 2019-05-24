1919491899221add1d4c065729035939a9056e48
linjj93 <linjj93@hotmail.com>
2019-05-24 21:03:58 +0800

    add event listeners to li and input box

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
-const tasks = [
-	'buy milk',
-	'eat dinner',
-	'nail javascript',
-	'give feedback',
-];
+const tasks = ["buy milk", "eat dinner", "nail javascript", "give feedback"];
+const toDoList = document.querySelector("#todo-list");
+const submitBtn = document.querySelector("button");
+const inputBox = document.querySelector("input");
+const editBtn = document.querySelector("ul li span");
+const itemInfo = submitBtn.previousElementSibling;
+
+for (let task of tasks) {
+  const listItem = document.createElement("li");
+  listItem.textContent = task;
+  toDoList.appendChild(listItem);
+  listItem.addEventListener("click", function() {
+    listItem.classList.toggle("done");
+  });
+}
+
+function addItem(taskInput, elementToAdd) {
+  if (!taskInput.value.trim() == "") {
+    elementToAdd.textContent = taskInput.value;
+    toDoList.appendChild(elementToAdd);
+    taskInput.value = null;
+    elementToAdd.addEventListener("click", function() {
+      elementToAdd.classList.toggle("done");
+    });
+  } else {
+    alert("No information found. Please enter details of to-do item.");
+  }
+}
+
+submitBtn.addEventListener("click", function() {
+  const listItem = document.createElement("li");
+  addItem(itemInfo, listItem);
+});
+
+inputBox.addEventListener("keypress", function(event) {
+  const listItem = document.createElement("li");
+  if (event.keyCode == 13) {
+    event.preventDefault();
+    addItem(itemInfo, listItem);
+  }
+});
